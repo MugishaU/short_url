a0e8377f1c6b3563bfd1da4465cc04b5a7374887
MugishaU <muwiragiye123@gmail.com>
2020-09-23 10:03:25 +0100

    add commands to allow deployment

--- short_url/short_url/settings.py
+++ short_url/short_url/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '6m9qz0^+#i8$266b&w80lfnh^^opl&bl$guvp=sa+g0zy=u#4*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -119,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+django_heroku.settings(locals())
