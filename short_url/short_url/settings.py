abd19dd53ae6fd827eabb472f5494be8170ae692
MugishaU <muwiragiye123@gmail.com>
2020-09-23 16:12:13 +0100

    update dependencies

--- short_url/short_url/settings.py
+++ short_url/short_url/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
-import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -120,6 +119,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-# PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-# STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
-# django_heroku.settings(locals())
