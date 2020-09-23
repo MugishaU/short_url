47c8c0e156db5b47ac5fcb1d8df54331c13d7f4a
MugishaU <muwiragiye123@gmail.com>
2020-09-23 11:13:09 +0100

    add paths for static files

--- short_url/short_url/settings.py
+++ short_url/short_url/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 import django_heroku
 from pathlib import Path
 
@@ -120,4 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 django_heroku.settings(locals())
