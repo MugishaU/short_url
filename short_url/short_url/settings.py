89d5a17e33fb725a86b460b96900edf8a5ceb416
MugishaU <muwiragiye123@gmail.com>
2020-10-05 21:06:04 +0100

    change debug to false and allowed host to heroku

--- short_url/short_url/settings.py
+++ short_url/short_url/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '6m9qz0^+#i8$266b&w80lfnh^^opl&bl$guvp=sa+g0zy=u#4*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com'
 
 
 # Application definition
